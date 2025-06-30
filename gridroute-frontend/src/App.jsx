54fa12c9a7963933d2ff30983cb882630574d002
riteshkumar800 <158203839+riteshkumar800@users.noreply.github.com>
2025-06-30 19:03:39 +0530

    Update App.jsx

--- gridroute-frontend/src/App.jsx
+++ gridroute-frontend/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
 
     try {
       setLoading(true);
-      const response = await fetch("http://127.0.0.1:8000/optimize-route", {
+      const response = await fetch('https://gridroute-project.onrender.com/optimize-route', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
